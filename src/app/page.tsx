37a62c5fab79d4fc6418e56c7e40ebcc2d8548bb
IdanLo <idan2110@gmail.com>
2025-02-01 03:05:58 +0200

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { Calendar } from '@/components/calendar';
 import { connectDB } from '@/lib/mongodb';
 import { Shift } from '@/models/Shift';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
     await connectDB();
     let events = await Shift.find();
