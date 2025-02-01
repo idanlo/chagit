b0cb91d91483d1b975020b090edbea7f3d0444f0
IdanLo <idan2110@gmail.com>
2025-02-01 03:03:22 +0200

    b

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,33 +3,6 @@ import { Calendar } from '@/components/calendar';
 import { connectDB } from '@/lib/mongodb';
 import { Shift } from '@/models/Shift';
 
-// const events = [
-//     {
-//         date: new Date(new Date().getFullYear(), new Date().getMonth(), 5),
-//         title: 'Team Meeting',
-//     },
-//     {
-//         date: new Date(new Date().getFullYear(), new Date().getMonth(), 5),
-//         title: 'Team Meeting2',
-//     },
-//     {
-//         date: new Date(new Date().getFullYear(), new Date().getMonth(), 5),
-//         title: 'Team Meeting3',
-//     },
-//     {
-//         date: new Date(new Date().getFullYear(), new Date().getMonth(), 10),
-//         title: 'Project Deadline',
-//     },
-//     {
-//         date: new Date(new Date().getFullYear(), new Date().getMonth(), 15),
-//         title: 'Birthday Party',
-//     },
-//     {
-//         date: new Date(new Date().getFullYear(), new Date().getMonth(), 20),
-//         title: 'Conference Call',
-//     },
-// ];
-
 export default async function Home() {
     await connectDB();
     let events = await Shift.find();
